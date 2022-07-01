508e1dee062a4276456df64d8d9a7540fb48bce9
Wajahat Saeed <108377128+Neroforte2021@users.noreply.github.com>
2022-07-01 05:49:12 +0500

    Add files via upload

---
+++ src/pages/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return(
+          <div >
+              <h2>Header hjfsdkjgfkl;gkfsdlgk</h2>
+              <p className="text-danger">
+hjkjhkghjgjhjhhgjhjhkjhkhkmnnknnbvhgvhb vbfghf hgfghfggh fgf ghf g gf 
+
+              </p>
+          </div>  
+        );
+    }
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './Header.jsx';
+
+class Home extends Component {
+    render() {
+        return(
+            
+          <div className="" >
+       
+              <h2>Home hjfsdkjgfkl;gkfsdlgk</h2>
+              <p className="text-danger">
+hjkjhkghjgjhjhhgjhjhkjhkhkmnnknnbvhgvhb vbfghf hgfghfggh fgf ghf g gf 
+
+              </p>
+          </div>  
+        );
+    }
+}
+
+export default Home;
